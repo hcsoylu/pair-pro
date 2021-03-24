2eec4ab13307a3a09014e4f99e114fea484bdd13
Mihai Ivanov <mikee2988@yahoo.com>
2021-03-24 16:21:48 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 // import logo from './logo.svg';
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import WarningSign from "./components/Alertcomp";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <WarningSign message="mihai" />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Alertcomp.jsx
@@ -0,0 +1,4 @@
+const WarningSign = (prop) => alert(prop.message);
+<Button variant="danger">Danger</Button>;
+
+export default WarningSign;
